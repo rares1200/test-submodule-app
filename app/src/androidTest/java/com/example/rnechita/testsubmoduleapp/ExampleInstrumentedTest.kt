2993998aea7a1148403ae3cd942fe65fc664f918
Rares NECHITA <rnechita@bitdefender.com>
2020-02-07 18:22:01 +0200

    Added new function in submodule

--- app/src/androidTest/java/com/example/rnechita/testsubmoduleapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/rnechita/testsubmoduleapp/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package com.example.rnechita.testsubmoduleapp
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.runner.AndroidJUnit4
+import hello.HelloManager
 
 import org.junit.Test
 import org.junit.runner.RunWith
